c92211876426373eaeef97f486a19ee01e1707be
Uwaifo Obed <Uwaifoobed1@gmail.com>
2025-02-10 21:54:42 +0100

    html and css update

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ async def home(request:Request):
 	* method : home
 	* description : Route to load the home page of our app
 	"""
-	return templates.TemplateResponse('index.html',{'request':request,'name':'QR CODE GENERATOR'})
+	return templates.TemplateResponse('qrcode.html',{'request':request,'name':'QR CODE GENERATOR'})
 
 """ @app.get('/createqr')
 async def createqr(file: UploadFile = File(...)):
